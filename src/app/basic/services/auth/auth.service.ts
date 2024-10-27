06da06e8dcbe652a9cdfe4b57951aa13933884ec
Anurag7524 <90264405+Anurag7524@users.noreply.github.com>
2024-10-27 17:23:19 +0530

    Update auth.service.ts

--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
-const BASIC_URL = "http://localhost:8080/"
+const BASIC_URL = "http://localhost:8080/";
+export const AUTH_HEADER = 'authorization';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +22,19 @@ export class AuthService {
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true  // Allows cookies and credentials to be sent
     });
+  }
+
+    login(username:string, password:string){
+    return this.http.post(BASIC_URL +  "authenticate", {username, password}, {observe:'response'})
+    .pipe(
+      map((res:HttpResponse<any>)=>{
+        console.log(res.body)
+        const tokenLength = res.headers.get(AUTH_HEADER)?.length;
+        const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
+        console.log(bearerToken);
+        return res;
+      })
+    );
   }
 
 }

7a89f8f2386b3190acc45d605396548cb1de3cdf
Anurag7524 <90264405+Anurag7524@users.noreply.github.com>
2024-10-31 19:22:32 +0530

    Update auth.service.ts

--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -17,11 +17,11 @@ export class AuthService {
     private userStorageService: UserStorageService) { }
 
   registerClient(signupRequestDTO:any): Observable<any> {
-    return this.http.post(BASIC_URL + "client/signup", signupRequestDTO);
+    return this.http.post(BASIC_URL + "client/sign-up", signupRequestDTO);
   }
 
   registerCompany(signupRequestDTO:any): Observable<any> {
-    return this.http.post(BASIC_URL + "company/signup", signupRequestDTO, {
+    return this.http.post(BASIC_URL + "company/sign-up", signupRequestDTO, {
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true  // Allows cookies and credentials to be sent
     });

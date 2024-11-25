2b9ed0b181c05a68fab96974e62f4bc67621826f
I528593 <anurag.rawat@sap.com>
2024-11-25 18:23:30 +0530

    Adding page size

--- src/app/client/pages/client-dashboard/client-dashboard.component.ts
+++ src/app/client/pages/client-dashboard/client-dashboard.component.ts
@@ -13,7 +13,7 @@ export class ClientDashboardComponent implements OnInit {
   
   // Pagination properties
   totalElements = 0;
-  pageSize = 1;
+  pageSize = 5;
   pageNumber = 0;
   searchTerm = '';
 
